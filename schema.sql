6c1f0c7ca68cba2bc08612b9a4221e3adc10f5e8
YunseokJANG <yunseok.jang@kaist.ac.kr>
2016-04-10 17:38:25 +0900

    - created 'topic' table - changed all DDL words to upper case letters. - included some indexes.

--- schema.sql
+++ schema.sql
@@ -6,47 +6,81 @@ drop table if exists words;
 drop table if exists sentences;
 drop table if exists posts;
 
-create table posts(
-    _id int primary key auto_increment,
-    url varchar(255) character set utf8mb4,
-    title varchar(255) character set utf8mb4
-);
-create table words(
-    _id int primary key auto_increment,
-    word varchar(255) character set utf8mb4
-);
-create table rulesets(
-    _id int primary key auto_increment,
-    name varchar(255) character set utf8mb4
+/* [static] topic of an analysis */
+CREATE TABLE topics(
+    _id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) CHARACTER SET UTF8MB4
 );
 
-create table rules(
-    _id int primary key auto_increment,
-    ruleset_id int,
-    full_text text character set utf8mb4,
-    foreign key (ruleset_id) references rulesets(_id)
+/* [static] We will use only few sources. Twitter / Instagram / YouTube / NewsInWeb. */
+CREATE TABLE sources(
+    _id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) CHARACTER SET UTF8MB4
 );
-create table sentences(
-    post_id int,
-    sentence_seq int,
-    full_text text character set utf8mb4,
-    primary key (post_id, sentence_seq),
-    foreign key (post_id) references posts(_id)
+
+/* vocab */
+CREATE TABLE words(
+    _id INT PRIMARY KEY AUTO_INCREMENT,
+    word VARCHAR(128) CHARACTER SET UTF8MB4,
+    UNIQUE KEY `idx_word` (`word`) USING HASH
 );
-create table sentence_word_relations(
-    post_id int,
-    sentence_seq int,
-    word_seq int,
-    word_id int,
-    primary key (post_id, sentence_seq, word_seq),
-    foreign key (post_id, sentence_seq) references sentences(post_id, sentence_seq),
-    foreign key (word_id) references words(_id)
+
+/* each post in a topic, crawled from one of the sources */
+CREATE TABLE posts(
+    _id INT PRIMARY KEY AUTO_INCREMENT,
+    topic_id INT,
+    source_id INT,
+    title VARCHAR(255) CHARACTER SET UTF8MB4,
+    url TEXT CHARACTER SET UTF8MB4,
+    FOREIGN KEY (topic_id) REFERENCES topics(_id),
+    FOREIGN KEY (source_id) REFERENCES sources(_id)
 );
-create table rule_word_relations(
-    rule_id int,
-    word_seq int,
-    word_id int,
-    primary key (rule_id, word_seq),
-    foreign key (rule_id) references rules(_id),
-    foreign key (word_id) references words(_id)
+
+/* each sentence in a post. Since sentence is the base unit for grading the rules, we decided to create this table */
+CREATE TABLE sentences(
+    post_id INT,
+    sentence_seq INT,
+    full_text TEXT CHARACTER SET UTF8MB4,
+    PRIMARY KEY (post_id, sentence_seq),
+    FOREIGN KEY (post_id) REFERENCES posts(_id)
+);
+
+/* store the word orders */
+CREATE TABLE sentence_word_relations(
+    post_id INT,
+    sentence_seq INT,
+    word_seq INT,
+    word_id INT,
+    PRIMARY KEY (post_id, sentence_seq, word_seq),
+    FOREIGN KEY (post_id, sentence_seq) REFERENCES sentences(post_id, sentence_seq),
+    FOREIGN KEY (word_id) REFERENCES words(_id)
+);
+
+/* [static] each topic has 4~6 rulesets. (most of them has 5 categories) */
+CREATE TABLE rulesets(
+    topic_id INT,
+    category_seq INT,   /* since it would be weird to see '103th ruleset' in screen, I changed this to 'category_number' */
+    name VARCHAR(255) CHARACTER SET UTF8MB4,
+    PRIMARY KEY (topic_id, category_seq),
+    FOREIGN KEY (topic_id) REFERENCES topics(_id)
+);
+
+/* each rules in a ruleset */
+CREATE TABLE rules(
+    _id INT PRIMARY KEY AUTO_INCREMENT,
+    topic_id INT,
+    category_seq INT,
+    full_text TEXT CHARACTER SET UTF8MB4,
+    INDEX `idx_topicid_categoryseq` (`topic_id` ASC, `category_seq` ASC),
+    FOREIGN KEY (topic_id, category_seq) REFERENCES rulesets(topic_id, category_seq)
+);
+
+/* store the word orders */
+CREATE TABLE rule_word_relations(
+    rule_id INT,
+    word_seq INT,
+    word_id INT,
+    PRIMARY KEY (rule_id, word_seq),
+    FOREIGN KEY (rule_id) REFERENCES rules(_id),
+    FOREIGN KEY (word_id) REFERENCES words(_id)
 );
