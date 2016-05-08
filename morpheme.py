3337be6af4bfe2bfcd953bc46d8f15f39f019fa4
sungmin <smoh2044@gmail.com>
2016-05-08 11:26:40 +0900

    UI

--- morpheme.py
+++ morpheme.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import MySQLdb as mdb
 import _mysql_exceptions
-import sys
+import ast
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
 import sys
 reload(sys)
@@ -24,6 +24,39 @@ app.config.update(dict(
     PASSWORD = 'default'
     ))
 app.config.from_envvar('FLASK_SETTINGS', silent=True)
+
+queries = {
+        'add_topic': 'INSERT INTO topics (name) VALUES (%s)',
+        'get_topics': 'SELECT _id, name FROM topics',
+
+        'add_source': 'INSERT INTO sources (name) VALUES (%s)',
+        'get_sources': 'SELECT _id, name FROM sources',
+
+        'add_post': 'INSERT INTO posts (topic_id, source_id, title, url) VALUES (%s, %s, %s, %s)',
+        'add_sentence': 'INSERT INTO sentences (post_id, sentence_seq, full_text) VALUES (%s, %s, %s)',
+        'add_word': 'INSERT IGNORE INTO words (word) VALUES (%s)',
+        'add_sentence_word_relation': 'INSERT INTO sentence_word_relations (post_id, sentence_seq, word_seq, word_id) \
+                        SELECT %s, %s, %s, _id FROM words WHERE word = %s',
+        'get_posts': 'SELECT B._id, A.full_text FROM sentences AS A INNER JOIN posts AS B ON A.post_id = B._id \
+                      WHERE B.topic_id = %s and B.source_id IN (%s)',
+
+        'add_ruleset': 'INSERT INTO rulesets (topic_id, category_seq, name) \
+                        SELECT %s, IFNULL(MAX(category_seq), 0) + 1, %s FROM rulesets WHERE topic_id = %s',
+        'get_ruleset': 'SELECT category_seq, name FROM rulesets WHERE topic_id = %s ORDER BY category_seq DESC LIMIT 1',
+        'get_rulesets': 'SELECT category_seq, name FROM rulesets WHERE topic_id = %s',
+        'del_ruleset': 'DELETE FROM rulesets WHERE topic_id = %s and category_seq = %s',
+        'del_rules': 'DELETE FROM rules WHERE topic_id = %s and category_seq = %s',
+        'del_rules_word_relations': 'DELETE FROM rule_word_relations WHERE rule_id in (SELECT _id FROM rules WHERE  topic_id = %s and category_seq = %s)',
+
+        'add_rule': 'INSERT INTO rules (topic_id, category_seq, full_text) VALUES (%s, %s, %s)',
+        'get_rules': 'SELECT rule_id, full_text, word FROM rules AS A INNER JOIN rule_word_relations AS B ON A._id = B.rule_id INNER JOIN words AS C ON B.word_id = C._id WHERE A.topic_id = %s AND A.category_seq = %s',
+        'del_rule': 'DELETE FROM rules WHERE _id = %s',
+        'del_rule_word_relations': 'DELETE FROM rule_word_relations WHERE rule_id = %s',
+
+        'add_rule_word_relation': 'INSERT INTO rule_word_relations (rule_id, word_seq, word_id) \
+                                   SELECT %s, %s, _id FROM words WHERE word = %s'
+            }
+
 
 # Should be modified when it be deployed.
 def connect_db():
@@ -50,40 +83,40 @@ def init_db():
 
 def tuplize(s):
     return (s,)
+
 def store_posts():
     db = connect_db()
+    cur = db.cursor()
+
+    cur.execute(queries['add_topic'], (u'총선', ))
+    topic_id = cur.lastrowid
+    cur.execute(queries['add_source'], ('naver', ))
+    source_id = cur.lastrowid
+
     files = crawl()
     print files;
     for fname in files:
-        cur = db.cursor()
         f = open(fname)
         print 'inserting %s' %fname
 
         # insert post
-        add_post = 'INSERT INTO posts (url, title) VALUES (%s, %s)'
-        cur.execute(add_post, ('dummy', fname))
+        cur.execute(queries['add_post'], (topic_id, source_id, fname, 'dummy_url'))
         post_id = cur.lastrowid
 
         lines = convert_textfile_to_lines(f)
         sentences = do_sentencing_without_threading(lines)
         for i in range (len(sentences)):
-            sentence = sentences[i]
             # insert sentence
-            add_sentence = 'INSERT INTO sentences (post_id, sentence_seq, full_text) VALUES (%s, %s, %s)'
-            cur.execute(add_sentence, (post_id, i, sentence))
+            sentence = sentences[i]
+            cur.execute(queries['add_sentence'], (post_id, i, sentence))
 
+            # insert newly appeared words. 
             morphs = map(concate_tuple, do_parsing_without_threading(sentence))
-            morphs_dedup = map(tuplize, dedup(morphs)) # can be ignored. need to check
-            
-            # insert newly appeared words. if there already is, idk, maybe no inserting
-            add_word = 'INSERT IGNORE INTO words (word) VALUES (%s)'
-            cur.executemany(add_word, morphs_dedup)
+            cur.executemany(queries['add_word'], map(tuplize, dedup(morphs)))
 
-            
+            # insert relations
             for j in range(len(morphs)):
-                add_relation = 'INSERT INTO sentence_word_relations (post_id, sentence_seq, word_seq, word_id) \
-                                SELECT %s, %s, %s, _id FROM words WHERE word = %s'
-                cur.execute(add_relation, (post_id, i, j, morphs[j]))
+                cur.execute(queries['add_sentence_word_relation'], (post_id, i, j, morphs[j]))
 
         db.commit()    
         print 'Done'
@@ -100,47 +133,122 @@ def teardown_request(exception):
 @app.route('/')
 def show_posts():
     cur = g.db.cursor()
-    cur.execute('select post_id, full_text from sentences')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template('show_posts.html', entries=entries)
+    cur.execute(queries['get_topics'])
+    topics = [dict(id=row[0], name=row[1]) for row in cur.fetchall()]
+    cur.execute(queries['get_sources'])
+    sources = [dict(id=row[0], name=row[1]) for row in cur.fetchall()]
+    return render_template('show_posts.html', topics=topics, sources=sources)
 
-@app.route('/parsing', methods=['GET'])
+@app.route('/_posts', methods=['GET'])
+def posts():
+    if request.method == 'GET':
+        topic = ast.literal_eval(request.args.get('topic'))
+        sources_ids = ', '.join(list(map(lambda x: '%s' %x, ast.literal_eval(request.args.get('sources')))))
+        cur = g.db.cursor()
+        cur.execute(queries['get_posts'], (topic, sources_ids))
+        posts = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+        cur.execute(queries['get_rulesets'], topic)
+        rulesets = [dict(category_seq=row[0], name=row[1]) for row in cur.fetchall()]
+        return jsonify(posts=posts, rulesets=rulesets)
+
+@app.route('/_rulesets', methods=['POST', 'DELETE'])
+def rulesets():
+    if request.method == 'POST':
+        topic = request.form['topic']
+        name = request.form['name']
+        db = g.db
+        cur = db.cursor()
+        cur.execute(queries['add_ruleset'], (topic, name, topic))
+        db.commit()
+        cur.execute(queries['get_ruleset'], (topic))
+        rulesets = [dict(category_seq=row[0], name=row[1]) for row in cur.fetchall()]
+        return jsonify(rulesets=rulesets)
+    if request.method == 'DELETE':
+        topic = request.form['topic']
+        category_seq = request.form['category_seq']
+        db = g.db
+        cur = db.cursor()
+        cur.execute(queries['del_rules_word_relations'], (topic, category_seq))
+        cur.execute(queries['del_rules'], (topic, category_seq))
+        cur.execute(queries['del_ruleset'], (topic, category_seq))
+        db.commit()
+        return jsonify(deleted=dict(topic=topic, category_seq=category_seq))
+
+
+@app.route('/_parsing', methods=['GET'])
 def parsing():
     jpype.attachThreadToJVM()
     text = request.args.get('text')
-    return jsonify(result=do_parsing_without_threading(text))
-    
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
+    return jsonify(morphs=do_parsing_without_threading(text))
+
+@app.route('/_rules', methods=['GET', 'POST', 'DELETE'])
+def rules():
     db = g.db
     cur = db.cursor()
-    cur.execute('insert into entries (title, text) values (%s, %s)',
-                 [request.form['title'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('you were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+        topic = request.form['topic']
+        category_seq = request.form['category_seq']
+        ruleText = request.form['ruleText']
+        checked = ast.literal_eval(request.form['checked'])
+        cur.executemany(queries['add_word'], map(tuplize, checked))
+        cur.execute(queries['add_rule'], (topic, category_seq, ruleText))
+        rule_id = cur.lastrowid
+        for seq, word in enumerate(checked):
+            cur.execute(queries['add_rule_word_relation'], (rule_id, seq+1, word))
+        db.commit()
+        return jsonify(checked=checked)
+    if request.method == 'GET':
+        topic = request.args.get('topic')
+        category_seq = request.args.get('category_seq')
+        cur.execute(queries['get_rules'], (topic, category_seq))
+        rulesDic = {}
+        for row in cur.fetchall():
+            rule_id, full_text, word = row
+            word = word[:word.rindex('_')]
+            if rule_id not in rulesDic:
+                rulesDic[rule_id] = (full_text, [word])
+            else:
+                rulesDic[rule_id][1].append(word)
+        rules = [dict(rule_id=key, full_text=val[0], word=val[1]) for key, val in rulesDic.iteritems()]
+        return jsonify(rules=rules)
+    if request.method == 'DELETE':
+        rule_id = request.form['rule_id']
+        cur.execute(queries['del_rule_word_relations'], (rule_id, ))
+        cur.execute(queries['del_rule'], (rule_id, ))
+        db.commit()
+        return jsonify(deleted=dict(rule_id=rule_id))
+    
+# @app.route('/add', methods=['POST'])
+# def add_entry():
+    # if not session.get('logged_in'):
+        # abort(401)
+    # db = g.db
+    # cur = db.cursor()
+    # cur.execute('INSERT INTO entries (title, text) VALUES (%s, %s)',
+                 # [request.form['title'], request.form['text']])
+    # db.commit()
+    # flash('New entry was successfully posted')
+    # return redirect(url_for('show_entries'))
 
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('you were logged out')
-    return redirect(url_for('show_entries'))
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+    # error = None
+    # if request.method == 'POST':
+        # if request.form['username'] != app.config['USERNAME']:
+            # error = 'Invalid username'
+        # elif request.form['password'] != app.config['PASSWORD']:
+            # error = 'Invalid password'
+        # else:
+            # session['logged_in'] = True
+            # flash('you were logged in')
+            # return redirect(url_for('show_entries'))
+    # return render_template('login.html', error=error)
+
+# @app.route('/logout')
+# def logout():
+    # session.pop('logged_in', None)
+    # flash('you were logged out')
+    # return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
     # init_db()
--- static/show_posts.js
+++ static/show_posts.js
@@ -1,45 +1,299 @@
-var create_checkbox = function(result) {
-    $.each(result, function (i, v) {
-        $('#checkboxes').append(
-            $('<div>').attr({'class': 'checkbox'}).prepend(
-                $('<label>').text(this[0]).prepend(
-                    $("<input>").attr({
-                        'type': 'checkbox', 
-                        'class' : 'inp-checkbox'
-                    }).val(this.join('_'))
+var topic;
+var sources;
+var ruleText;
+var ishover;
+//var category_seq;
+
+var create_rule_checkbox = function(morphs) {
+    $.each(morphs, function(){
+        var morph = this;
+        var word = this[0];
+        $('#rule-checkboxes').append(
+            $('<div>').attr({
+                'class': 'checkbox',
+                'style': 'display:inline-block; margin:5px'
+            }).append(
+                $('<label>').text(word).prepend(
+                    $('<input>').attr({
+                        'type': 'checkbox',
+                        'class' : 'word-checkbox',
+                        'name': 'word-selection'
+                    }).val(morph.join('_'))
                 )
+            )
+        )
+    });
+};
+var get_checked_rules = function(category_seq){
+    var ret = [];
+    $('input:checkbox[name=word-selection]:checked').each(function(){
+        ret.push($(this).val());
+    });
+    return ret;
+};
+var empty_checkboxes = function(){
+    $('#rule-checkboxes').empty();
+};
+
+var show_posts = function(posts){
+    $.each(posts, function(){
+        $('#posts').append(
+            $('<tr>').prepend(
+                $('<td>').text(this.title),
+                $('<td>').text(this.text)
             )
         );
     });
 };
 
-var create_register_button = function() {
-    $('#checkboxes').append(
-        $('<button>').text('Register').attr({
-            'type': 'button',
-            'class': 'btn btn-default',
-            'id': 'register'
-        })
-    )
-};
-
-var main = function() {
-    var httpRequest;
-    $('#parse').click(function() {
+var show_rules = function(category_seq, rules){
+    $('#ruleset-menu-'+category_seq).empty();
+    $.each(rules, function(){
+        var rule_id= this.rule_id;
+        var full_text = this.full_text;
+        var words = this.word;
+        $('#ruleset-menu-'+category_seq).append(
+            $('<li>').attr({
+                'id':'rule-li-'+rule_id
+            }).append(
+                $('<a>').attr({
+                    'class':'clearfix',
+                    'id':'rule-a-'+rule_id 
+                }).text(full_text).append(
+                    $('<span>').attr({'class':'pull-right'}).append(
+                        $('<button>').attr({
+                            'class':'btn btn-warning btn-xs glyphicon glyphicon-remove ruleset-button',
+                            'value':rule_id,
+                            'name':'delete-rule-btn'
+                        })
+                    )
+                )
+            )
+        );
+        $.each(words, function(){
+            $('#rule-a-'+rule_id).append(
+                $('<span>').attr({
+                    'class':'label label-default',
+                    'style':'margin:3px'
+                }).text(this)
+            )
+        });
+    });
+    $(document).on('click', 'button[name=delete-rule-btn]', function(){
+        var rule_id = this.value
         jQuery.ajax({
-            type: 'GET',
-            url: '/parsing',
-            data: {'text': $('#rule_text').val()},
+            type: 'DELETE',
+            url: '/_rules',
+            data: {
+                'rule_id':rule_id 
+            },
             dataType: 'JSON',
             success: function(data){
-                create_checkbox(data.result);
-                create_register_button();
+                $('#rule-li-'+rule_id).remove()
             },
             error: function(xhr, status, error){
-                alert('errrrrrrr');
+                console.log('get rules failed');
             }
         });
     });
+
+}
+
+var show_rulesets = function(rulesets){
+    $('#ruleset-creator-ul').css('display', 'block');
+    $.each(rulesets, function(){
+        var category_seq = this.category_seq;
+        var name = this.name;
+        $('#rulesets').prepend(
+            $('<li>').attr({
+                'class':'dropdown',
+                'name':'ruleset-dropdown',
+                'id':'ruleset-dropdown-'+category_seq
+            }).append(
+                $('<a>').attr({
+                    'class':'dropdown-toggle clearfix',
+                    'data-toggle':'dropdown',
+                    'name':'ruleset-a',
+                    'value':category_seq,
+                    'id':'ruleset-a-'+category_seq
+                }).text(name).append(
+                    $('<span>').attr({'class':'pull-right'}).append(
+                        $('<button>').attr({
+                            'class':'btn btn-info btn-xs glyphicon glyphicon-file ruleset-button',
+                            'style':'margin-right:5px',
+                            'value':category_seq,
+                            'name':'create-rule-modal-btn',
+                            'data-toggle':'modal',
+                            'data-target':'#create-rule-modal'
+                        }),
+                        $('<button>').attr({
+                            'class':'btn btn-warning btn-xs glyphicon glyphicon-remove ruleset-button',
+                            'value':category_seq,
+                            'name':'delete-ruleset-btn'
+                        })
+                    )
+                ),
+                $('<ul>').attr({
+                    'class':'dropdown-menu navmenu-nav',
+                    'name':'ruleset-menu',
+                    'id':'ruleset-menu-'+category_seq,
+                    'role':'menu'
+                })
+            )
+        );
+    });
+    $(document).on('click', 'a[name=ruleset-a]', function(){
+        if(ishover){return}
+        var category_seq = this.getAttribute('value');
+        jQuery.ajax({
+            type: 'GET',
+            url: '/_rules',
+            data: {
+                'topic': topic,
+                'category_seq': category_seq 
+            },
+            dataType: 'JSON',
+            success: function(data){
+                show_rules(category_seq, data.rules)
+            },
+            error: function(xhr, status, error){
+                console.log('get rules failed');
+            }
+        });
+    });
+    $(document).on('click', 'button[name=create-rule-modal-btn]', function(){
+        $('#parse').attr({'value': this.value});
+    });
+    $(document).on('click', 'button[name=delete-ruleset-btn]', function(){
+        var category_seq = this.value
+        jQuery.ajax({
+            type: 'DELETE',
+            url: '/_rulesets',
+            data: {
+                'topic': topic,
+                'category_seq': category_seq
+            },
+            dataType: 'JSON',
+            success: function(data){
+                $('#ruleset-a-'+category_seq).empty()
+                $('#ruleset-a-'+category_seq).remove()
+            },
+            error: function(xhr, status, error){
+                console.log('get rules failed');
+            }
+        });
+    });
+    $(document).on({
+            'mouseenter': function(){
+                //$('a[name=ruleset-a]').click(function(){return false});
+                ishover = true;
+            },
+            'mouseleave': function(){
+                //$('a[name=ruleset-a]').unbind('click');
+                ishover = false;
+            }
+        }, '.ruleset-button'
+    );
+};
+
+var clear_posts = function(){
+    $('#posts').empty()
+}
+var clear_rulesets = function(){
+    $('#rulesets').empty()
+}
+
+var main = function(){
+    var httpRequest;
+    $('#sources').selectpicker('selectAll');
+    $('#getPosts').click(function(){
+        topic = $("#topics").val();
+        sources = $("#sources").val();
+        jQuery.ajax({
+            type: 'GET',
+            url: '/_posts',
+            data: {
+                'topic': JSON.stringify(topic),
+                'sources': JSON.stringify(sources)
+            },
+            dataType: 'JSON',
+            success: function(data){
+                clear_posts();
+                clear_rulesets();
+                show_posts(data.posts);
+                show_rulesets(data.rulesets);
+            },
+            error: function (xhr, status, error){
+                console.log('get posts ajax errrr')
+            }
+        });
+    });
+    $('#ruleset-creator-btn').click(function(){
+        rulesetName = $('#ruleset-creator-txt').val();
+        jQuery.ajax({
+            type: 'POST',
+            url: '/_rulesets',
+            data: {
+                'topic': topic,
+                'name': rulesetName
+            },
+            dataType: 'JSON',
+            success: function(data){
+                show_rulesets(data.rulesets);
+                $('#ruleset-creator-txt').val('')
+            },
+            error: function(xhr, status, error){
+            }
+        });
+    });
+    $('#parse').click(function(){
+        ruleText = $('#rule-text').val();
+        jQuery.ajax({
+            type: 'GET',
+            url: '/_parsing',
+            data: {'text': ruleText},
+            dataType: 'JSON',
+            success: function(data){
+                empty_checkboxes()
+                create_rule_checkbox(data.morphs);
+            },
+            error: function(xhr, status, error){
+                console.log('parse ajax errrrrrrr');
+            }
+        });
+    });
+    $('#create-rule-reset-btn').click(function(){
+        ruleText = '';
+        $('#rule-text').val('');
+        empty_checkboxes()
+    });
+
+    $('#create-rule-btn').click(function(){
+        checked = get_checked_rules();
+        var category_seq = $('#parse').val();
+        jQuery.ajax({
+            type: 'POST',
+            url: '/_rules',
+            data: {
+                'topic': topic,
+                'category_seq': category_seq,
+                'ruleText': ruleText,
+                'checked': JSON.stringify(checked)
+            },
+            dataType: 'JSON',
+            success: function(data){
+                console.log(data.checked);
+            },
+            error: function(xhr, status, error){
+                console.log('rule is not posted');
+            }
+        });
+    });
+
+    $('#run').click(function(){
+    });
+
 };
 
 $(document).ready(main);
+
